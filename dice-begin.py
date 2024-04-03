ee75e55d4043980d270cd97685bd3a3fcba80c01
sparky9228 <107233151+sparky9228@users.noreply.github.com>
2024-04-02 17:45:25 -0700

    Add files via upload

--- dice-begin.py
+++ dice-begin.py
@@ -1,1 +1,17 @@
-print("the first file was cursed")
+import random
+def main():
+    numbers = [0,1,2,3,4,5,6,7,8,9,10,11]
+
+    input("Press enter to play: ")
+
+    player1 = random.randint(0,11)
+    player2 = random.randint(0,11)
+
+    print(f"player one's number is {numbers[player1]}\n player two's number is {numbers[player2]}")
+
+    if player1 > player2 :
+        print("player 1 wins !")
+    elif player2 > player1:
+        print ("player 2 wins!")
+    else:
+        print("it's a tie.. nobody wins.")
